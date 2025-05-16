b0ea14e9efe10441565e5e986d278555725da5ea
AmirProm <promgrameramir@gmail.com>
2025-05-16 19:36:00 +0330

    add Repositories

--- api/Interfaces/IAccountRepository.cs
+++ api/Interfaces/IAccountRepository.cs
@@ -9,7 +9,5 @@ namespace api.interfaces;
 public interface IAccountRepository
 {
     public Task<Appuser> RegisterAsync(Appuser userInput, CancellationToken cancellationToken);
-      public Task<Appuser> GetAllAsync(CancellationToken cancellationToken);
-
-
+     public Task<Appuser> GetAllAsync(CancellationToken cancellationToken);
 }
