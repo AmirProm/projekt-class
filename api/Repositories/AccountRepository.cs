281d0a54cb21d9e2f64e1b0c5cdbcbdec81cda95
AmirProm <promgrameramir@gmail.com>
2025-05-17 00:11:03 +0330

    add ID in models

--- api/Repositories/AccountRepository.cs
+++ api/Repositories/AccountRepository.cs
@@ -26,7 +26,7 @@ public class AccountRepository : IAccountRepository
 public async Task<Appuser?> RegisterAsync(Appuser userInput, CancellationToken cancellationToken)
     {
         Appuser user = await _collection.Find<Appuser>(doc =>
-        doc.Email == userInput.Email).FirstOrDefaultAsync(cancellationToken);
+        doc.Email == userInput.Email.Trim().ToLower()).FirstOrDefaultAsync(cancellationToken);
 
         if (user is not null)
             return null;
