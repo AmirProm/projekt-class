ad6673670d6a33d108ea4b11240b856b9ff5b0bf
AmirProm <promgrameramir@gmail.com>
2025-05-17 00:12:13 +0330

    add  DataAnnotations

--- api/Model/Appuser.cs
+++ api/Model/Appuser.cs
@@ -5,8 +5,8 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace api.model;
 
 public record Appuser(
- [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
+  [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
   string? Id,
-   [EmailAddress] string Email,
+  [EmailAddress] string Email,
   [MaxLength(15)] [MinLength(2)]string Name
 );
