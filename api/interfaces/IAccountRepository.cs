c9b7c5163e4f54e504840a42ce5fc68d27172292
AmirProm <promgrameramir@gmail.com>
2025-05-16 19:33:11 +0330

    interface register

---
+++ api/interfaces/IAccountRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.model;
+
+namespace api.interfaces;
+
+public interface IAccountRepository
+{
+    public Task<Appuser> RegisterAsync(Appuser userInput, CancellationToken cancellationToken);
+
+
+}
