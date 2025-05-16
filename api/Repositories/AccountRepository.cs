4f73575c0e88143cadb01e1f51b4f2283dfd12d0
AmirProm <promgrameramir@gmail.com>
2025-05-16 19:48:41 +0330

    add mongo in repository

--- api/Repositories/AccountRepository.cs
+++ api/Repositories/AccountRepository.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.interfaces;
 using api.model;
+using api.Settings;
+using MongoDB.Driver;
 
 namespace api.Repositories;
 
 public class AccountRepository : IAccountRepository
 {
+        private readonly IMongoCollection<Appuser> _collection;
+
+    // Dependency Injection
+    public AccountRepository(IMongoClient client, IMongoDbSettings dbSettings)
+    {
+        var dbName = client.GetDatabase(dbSettings.DatabaseName);
+        _collection = dbName.GetCollection<Appuser>("users");
+    }
 
     public Task<Appuser> GetAllAsync(CancellationToken cancellationToken)
     {
