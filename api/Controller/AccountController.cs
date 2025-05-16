4fe874ba3296b25cdbfb9ec9d0676ff59f12976d
AmirProm <promgrameramir@gmail.com>
2025-05-16 21:38:26 +0330

    Controler Register

--- api/Controller/AccountController.cs
+++ api/Controller/AccountController.cs
@@ -1,14 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using api.interfaces;
+using api.model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controller;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AccountController : ControllerBase
+public class AccountController(IAccountRepository accountRepository) : ControllerBase
 {
-    
+
+    [HttpPost("Register")]
+    public async Task<ActionResult<Appuser>> Register(Appuser userInput, CancellationToken cancellationToken)
+    {
+        Appuser? appuser = await accountRepository.RegisterAsync(userInput, cancellationToken);
+
+        if (appuser is null)
+            return BadRequest("email already.");
+
+        return appuser;
+    }
+
+
 }
